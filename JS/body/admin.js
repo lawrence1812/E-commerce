7b024291d18e0064a151f6473a935a49e594ef6e
lawrence1812 <lawrence.canton@gmail.com>
2023-03-07 22:24:49 +0100

    aggiunta la parte di XML

--- JS/body/admin.js
+++ JS/body/admin.js
@@ -1,11 +1,12 @@
-const listFields = ["product_id", "product_name", "price", "description", "associated_image"];
-const listTypeFields = ['number', 'text', 'number', 'text', 'file'];
+const listFields = ["product_name", "price", "description", "associated_image"];
+const listTypeFields = ['text', 'number', 'text', 'file'];
 
 const genTb = () => {
-
+    const main = document.createElement('main');
     const mainContent = document.createElement('div');
     mainContent.classList.add('mainContent');
-    document.body.appendChild(mainContent);
+    document.body.appendChild(main);
+    main.appendChild(mainContent);
 
     const flTable = document.createElement('table');
     flTable.classList.add('fl-table');
@@ -48,6 +49,7 @@ const genTb = () => {
             if (j == 1) {
                 td = document.createElement('td');
                 input = document.createElement('input');
+                input.id = listFields[i];
                 input.type = listTypeFields[i];
                 td.appendChild(input);
                 tr.appendChild(td);
@@ -56,4 +58,48 @@ const genTb = () => {
         }
 
     }
+    const button = document.createElement('button');
+    button.setAttribute('onClick', 'parserXML()');
+    button.textContent = 'Invio';
+    main.appendChild(button);
+
+}
+
+const parserXML = () => {
+    let xmlString = '<?xml version="1.0" encoding="UTF-8"?> <data>';
+
+    for (let i = 0; i < listFields.length; i++) {
+        const input = document.getElementById(listFields[i]).value;
+        const field = listFields[i];
+        xmlString += ` <${field}> ${input} </${field}> `;
+
+    }
+
+    xmlString += '</data>';
+    console.log(xmlString);
+
+
+    const blob = new Blob([xmlString], { type: 'application/xml' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'dati.xml';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+    /*
+    const parser = new DOMParser();
+
+    // Analizza la stringa XML e restituisce un documento DOM
+    const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+
+    // Recupera i dati dal documento DOM
+    const name = xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    const age = xmlDoc.getElementsByTagName("age")[0].childNodes[0].nodeValue;
+
+    // Stampa i dati
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    */
 }
