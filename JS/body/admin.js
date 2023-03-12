987dcc056f285a7be3c7256ff30572fad6fa081c
lawrence1812 <lawrence.canton@gmail.com>
2023-03-12 19:59:49 +0100

    .

--- JS/body/admin.js
+++ JS/body/admin.js
@@ -1,5 +1,11 @@
-const listFields = ["product_name", "price", "description", "associated_image"];
-const listTypeFields = ['text', 'number', 'text', 'file'];
+
+
+const listFields = [
+    { name: 'product_name', type: 'text' },
+    { name: 'price', type: 'number' },
+    { name: 'description', type: 'text' },
+    { name: 'associated_image', type: 'file' }
+]
 
 const genTb = () => {
     const main = document.createElement('main');
@@ -19,8 +25,6 @@ const genTb = () => {
     thTable.appendChild(trFr);
 
 
-    const row = listFields.length;
-
     let td;
 
     td = document.createElement('td');
@@ -35,71 +39,130 @@ const genTb = () => {
     flTable.appendChild(tbTable);
 
     let tr;
-    for (let i = 0; i < row; i++) {
+    for (let i = 0; i < listFields.length; i++) {
         tr = document.createElement('tr');
         tbTable.appendChild(tr);
         for (let j = 0; j < 2; j++) {
             if (j == 0) {
                 td = document.createElement('td');
                 span = document.createElement('span');
-                span.textContent = listFields[i];
+                span.textContent = listFields[i].name;
                 td.appendChild(span);
                 tr.appendChild(td);
             }
             if (j == 1) {
                 td = document.createElement('td');
                 input = document.createElement('input');
-                input.id = listFields[i];
-                input.type = listTypeFields[i];
+                input.id = listFields[i].name;
+                input.type = listFields[i].type;
                 td.appendChild(input);
                 tr.appendChild(td);
             }
+        }
+    }
+
+
+
+    /* Create popup */
+    const divContsainerPopup = document.createElement('div');
+    divContsainerPopup.id = 'popup-container';
+    main.appendChild(divContsainerPopup);
+
+
+    const buttonPopup = document.createElement('button');
+    buttonPopup.id = 'open-popup';
+    buttonPopup.textContent = 'Invio'
+    buttonPopup.onclick = () => {
+        divContsainerPopup.style.display = 'block';
+
+        const popup = document.createElement('div');
+        popup.id = 'popup';
+        divContsainerPopup.appendChild(popup);
+
+        for (let i = 0; i < listFields.length; i++) {
+
+            const input = document.getElementById(listFields[i].name).value || 'nessun inserimento';
+            const boxContent = document.createElement('div');
+            boxContent.className = 'box-content';
+            popup.appendChild(boxContent);
+            span = document.createElement('span');
+            if (input ?? 0) span.textContent = listFields[i].name + ' : ' + input;
+            boxContent.appendChild(span);
+
+            const btContent = document.createElement('div');
+            btContent.style.marginLeft = '2rem';
+            boxContent.appendChild(btContent);
+
+            const btClone = document.createElement('button');
+
+            btClone.addEventListener('click', (event) => {
+                const f = event.target.parentNode.parentNode;
+                const clNode = f.cloneNode(true);
+                document.querySelector('#popup').insertBefore(clNode, f.nextSibling);
+            });
+            const cloneIcon = document.createElement('i');
+            cloneIcon.className = 'fa fa-clone';
+            btContent.appendChild(btClone);
+            btClone.appendChild(cloneIcon);
+
+            const btRemove = document.createElement('button');
+            btRemove.onclick = () => {
+
+                document.querySelector('#popup').removeChild(boxContent);
+            }
+            const removeIcon = document.createElement('i');
+            removeIcon.className = 'fa fa-user';
+
+            btContent.appendChild(btRemove);
+            btRemove.appendChild(removeIcon);
+
+
 
         }
 
+        const buttonClosePopup = document.createElement('button');
+        buttonClosePopup.id = 'open-popup';
+        buttonClosePopup.textContent = 'Chiudi'
+        buttonClosePopup.onclick = () => {
+            divContsainerPopup.style.display = 'none';
+            divContsainerPopup.removeChild(popup);
+        }
+        popup.appendChild(buttonClosePopup);
+
+        const buttonOKAY = document.createElement('button');
+        buttonOKAY.addEventListener('click', () => {
+            let xmlString = '<?xml version="1.0" encoding="UTF-8"?> <data>';
+
+            for (let i = 0; i < listFields.length; i++) {
+                const input = document.getElementById(listFields[i].name).value;
+                const field = listFields[i].name;
+                xmlString += ` <${field}> ${input} </${field}> `;
+            }
+
+            xmlString += '</data>';
+
+            const blob = new Blob([xmlString], { type: 'application/xml' });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'dati.xml';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+
+        });
+        buttonOKAY.textContent = 'Conferma';
+        popup.appendChild(buttonOKAY);
     }
-    const button = document.createElement('button');
-    button.setAttribute('onClick', 'parserXML()');
-    button.textContent = 'Invio';
-    main.appendChild(button);
+    main.appendChild(buttonPopup);
+
+
 
 }
 
-const parserXML = () => {
-    let xmlString = '<?xml version="1.0" encoding="UTF-8"?> <data>';
-
-    for (let i = 0; i < listFields.length; i++) {
-        const input = document.getElementById(listFields[i]).value;
-        const field = listFields[i];
-        xmlString += ` <${field}> ${input} </${field}> `;
-
-    }
-
-    xmlString += '</data>';
-    console.log(xmlString);
 
 
-    const blob = new Blob([xmlString], { type: 'application/xml' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'dati.xml';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-    /*
-    const parser = new DOMParser();
 
-    // Analizza la stringa XML e restituisce un documento DOM
-    const xmlDoc = parser.parseFromString(xmlString, "text/xml");
 
-    // Recupera i dati dal documento DOM
-    const name = xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
-    const age = xmlDoc.getElementsByTagName("age")[0].childNodes[0].nodeValue;
 
-    // Stampa i dati
-    console.log("Name: " + name);
-    console.log("Age: " + age);
-    */
-}
